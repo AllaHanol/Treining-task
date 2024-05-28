488e3b87859a44634d46fa3f10d230310002aad3
AllaHanol <156720109+AllaHanol@users.noreply.github.com>
2024-05-28 21:41:26 +0200

    add

--- js/trening.js
+++ js/trening.js
@@ -1,8 +1,62 @@
-const Counter = {
-    data(){
-        return{
-            counter: 0
-        }
-    }
-}
-Vue.createApp(Counter).mount('#app')
+// Станція з продажу ремонтних дроїдів готова до запуску, залишилося написати програмне забезпечення для відділу продажів. Оголоси функцію makeTransaction(quantity, pricePerDroid, customerCredits), яка складає та повертає повідомлення про купівлю ремонтних дроїдів.
+
+// Вона оголошує три параметри, значення яких будуть задаватися під час її виклику:
+// quantity — кількість замовлених дроїдів
+// pricePerDroid — ціна одного дроїда
+// customerCredits — сума коштів на рахунку клієнта
+
+// Доповни функцію таким чином:
+// Оголоси змінну для зберігання загальної суми замовлення (загальна вартість усіх замовлених дроїдів) і задай їй вираз розрахунку цієї суми.
+// Додай перевірку, чи зможе клієнт оплатити замовлення:
+// якщо сума до сплати перевищує кількість кредитів на рахунку клієнта, функція має повертати рядок "Insufficient funds!"
+// в іншому випадку функція має повертати рядок "You ordered <quantity> droids worth <totalPrice> credits!", де <quantity> це кількість замовлених дроїдів, а <totalPrice> це їх загальна вартість.
+
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
+
+// function makeTransaction(quantity, pricePerDroid, customerCredits) {
+//     let totalPrice =(quantity * pricePerDroid);
+    // if(customerCredits >= totalPrice){return`"You ordered ${quantity} droids worth ${totalPrice} credits!"`
+    // } 
+    // else {return`"Insufficient funds!"`
+    // }
+//     return customerCredits >= totalPrice ? `"You ordered ${quantity} droids worth ${totalPrice} credits!"`:`"Insufficient funds!"`;
+// }
+// console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+// console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+// console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+// console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+// console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+
+// Оголоси функцію formatMessage(message, maxLength), яка приймає рядок (параметр message) та перевіряє його довжину відповідно до заданої максимальної довжини (параметр maxLength).
+
+// Доповни код функції таким чином, що:
+// Якщо довжина рядка дорівнює або менша за maxLength, то функція повертає початковий рядок без змін.
+// Якщо довжина перевищує maxLength, то функція обрізає рядок до maxLength символів, додає трикрапку "..." в кінці та повертає обрізану версію
+// function formatMessage(message, maxLength) {
+//     message = message.length <= maxLength ? message : maxLength.slice.
+// }
+
+// console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+// console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+// console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+// console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
+
+const hours = 14;
+const minutes = 26;
+let timeString;
+if(minutes===0){
+    timeString=`${hours} г.`
+} else {`'${hours} г. ${minutes} хв.'`}
+
+console.log(timeString);
+const dog = {
+    name: "Mango",
+    age: 3,
+    isGoodBoy: true,
+  };
+  
+  const json = JSON.stringify(dog);
+  console.log(json); // '{"name":"Mango","age":3,"isGoodBoy":true}'
+  
